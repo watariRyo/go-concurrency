8c3f67346f5b5494ff5e8e1addc4ce22ba6b5eaa
watariRyo <72895473+watariRyo@users.noreply.github.com>
2023-09-20 19:19:44 +0900

    add channel sample

---
+++ simple-channels/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func shout(ping <-chan string, pong chan<- string) {
+	for {
+		s := <-ping
+		pong <- fmt.Sprintf("%s!!!", strings.ToUpper(s))
+	}
+}
+
+func main() {
+	// create to channels
+	ping := make(chan string)
+	pong := make(chan string)
+
+	go shout(ping, pong)
+
+	fmt.Println("Type something and press ENTER (enter Q to quit)")
+
+	for {
+		// print a prompt
+		fmt.Print("-> ")
+
+		// get user input
+		var userInput string
+		_, _ = fmt.Scanln(&userInput)
+
+		if userInput == strings.ToLower("q") {
+			break
+		}
+
+		ping <- userInput
+		// wait a response
+		response := <-pong
+		fmt.Println("Response: ", response)
+	}
+	fmt.Println("All done. Closing channels")
+	close(ping)
+	close(pong)
+}
